7217f3d7fd0fd0d73e4cfa6b4c295f10551d5af7
xiaogoubuli <xiaogoubuli@163.com>
2019-08-18 13:01:39 +0800

    update cloud-database-h2 src

---
+++ cloud-database/cloud-database-h2/src/main/java/org/j2ee/cloud/database/h2/CloudDatabaseH2Application.java
@@ -0,0 +1,13 @@
+package org.j2ee.cloud.database.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDatabaseH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDatabaseH2Application.class, args);
+    }
+
+}

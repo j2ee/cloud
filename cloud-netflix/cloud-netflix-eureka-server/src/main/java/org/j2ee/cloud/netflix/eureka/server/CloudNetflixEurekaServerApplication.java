c1ee2291a647b3cb213073530b2b278acc4a8528
xiaogoubuli <xiaogoubuli@163.com>
2019-12-12 12:42:31 +0800

    update cloud-netflix-eureka-server src

---
+++ cloud-netflix/cloud-netflix-eureka-server/src/main/java/org/j2ee/cloud/netflix/eureka/server/CloudNetflixEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package org.j2ee.cloud.netflix.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNetflixEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNetflixEurekaServerApplication.class, args);
+    }
+
+}

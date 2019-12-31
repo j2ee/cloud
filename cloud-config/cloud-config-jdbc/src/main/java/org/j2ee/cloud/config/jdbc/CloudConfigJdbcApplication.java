cfa975dbc7032dd187412ca2cdaf99e9ea7ad791
xiaogoubuli <xiaogoubuli@163.com>
2019-12-31 14:14:01 +0800

    update cloud-config-jdbc src

---
+++ cloud-config/cloud-config-jdbc/src/main/java/org/j2ee/cloud/config/jdbc/CloudConfigJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.j2ee.cloud.config.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigJdbcApplication.class, args);
+    }
+
+}

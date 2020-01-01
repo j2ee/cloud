8394a107b07d342c8ada6a9d7ae9a6cd941a2170
xiaogoubuli <xiaogoubuli@163.com>
2020-01-01 13:18:21 +0800

    update cloud-config-git src

---
+++ cloud-config/cloud-config-git/src/main/java/org/j2ee/cloud/config/git/CloudConfigGitApplication.java
@@ -0,0 +1,13 @@
+package org.j2ee.cloud.config.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigGitApplication.class, args);
+    }
+
+}

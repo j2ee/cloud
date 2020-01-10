c705d16b9c4f999379fdbac6752361a394ce600e
xiaogoubuli <xiaogoubuli@163.com>
2020-01-10 10:22:01 +0800

    update cloud-config-file src

---
+++ cloud-config/cloud-config-file/src/main/java/org/j2ee/cloud/config/file/CloudConfigFileApplication.java
@@ -0,0 +1,13 @@
+package org.j2ee.cloud.config.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigFileApplication.class, args);
+    }
+
+}

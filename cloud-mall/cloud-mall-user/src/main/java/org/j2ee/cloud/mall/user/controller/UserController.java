d8da496bbf6d3fff4afc7a1c54af1c809611daf9
xiaogoubuli <xiaogoubuli@163.com>
2020-02-06 14:11:36 +0800

    update cloud-config-server-native

--- cloud-mall/cloud-mall-user/src/main/java/org/j2ee/cloud/mall/user/controller/UserController.java
+++ cloud-mall/cloud-mall-user/src/main/java/org/j2ee/cloud/mall/user/controller/UserController.java
@@ -1,18 +1,24 @@
 package org.j2ee.cloud.mall.user.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.j2ee.cloud.mall.user.feign.OrderService;
 import org.j2ee.cloud.mall.user.model.Order;
 import org.j2ee.cloud.mall.user.model.User;
 import org.j2ee.cloud.mall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/users")
+@Slf4j
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Value("${msg:world}")
+    private String msg;
 
     private UserService userService;
 
@@ -28,6 +34,12 @@ public class UserController {
     @GetMapping("/{id}")
     public Order getOrder(@PathVariable int id) {
         return orderService.getOrder(id);
+    }
+
+    @GetMapping("/msg")
+    public String getMsg() {
+        log.info("msg: {}", msg);
+        return msg;
     }
 
 }

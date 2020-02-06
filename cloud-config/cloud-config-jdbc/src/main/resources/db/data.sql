33ec21d85dcafcd6bf2264fba7f34753358d087f
xiaogoubuli <xiaogoubuli@163.com>
2020-02-06 23:09:13 +0800

    refactor

--- cloud-config/cloud-config-jdbc/src/main/resources/db/data.sql
+++
@@ -1,9 +0,0 @@
-insert into config_properties (id, `key`, `value`, application, profile, label)
-  values('1', 'my.name', 'lhd', 'config', 'dev', 'master');
-
-insert into user (use_id,use_name,use_sex,use_age,use_id_no,use_phone_num,use_email,create_time,modify_time,use_state) values(
-1,'赵一','0',20,'142323198610051234','12345678910','qe259@163.com',sysdate,sysdate,'0');
-insert into user (use_id,use_name,use_sex,use_age,use_id_no,use_phone_num,use_email,create_time,modify_time,use_state) values(
-2,'钱二','0',22,'142323198610051235','12345678911','qe259@164.com',sysdate,sysdate,'0');
-insert into user (use_id,use_name,use_sex,use_age,use_id_no,use_phone_num,use_email,create_time,modify_time,use_state) values(
-3,'孙三','1',24,'142323198610051236','12345678912','qe259@165.com',sysdate,sysdate,'0');

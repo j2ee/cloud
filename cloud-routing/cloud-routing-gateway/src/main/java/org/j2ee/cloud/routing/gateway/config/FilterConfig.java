c8e923da86228e6200a194445fc4ff404596fb7d
xiaogoubuli <xiaogoubuli@163.com>
2020-02-16 18:47:12 +0800

    add filterconfig

---
+++ cloud-routing/cloud-routing-gateway/src/main/java/org/j2ee/cloud/routing/gateway/config/FilterConfig.java
@@ -0,0 +1,47 @@
+package org.j2ee.cloud.routing.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter a() {
+        return (exchange, chain) -> {
+            log.info("first pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(
+                    () -> log.info("third post filter")
+            ));
+        };
+    }
+
+    @Bean
+    @Order(0)
+    public GlobalFilter b() {
+        return (exchange, chain) -> {
+            log.info("second pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(
+                    () -> log.info("second post filter")
+            ));
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public GlobalFilter c() {
+        return (exchange, chain) -> {
+            log.info("third pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(
+                    () -> log.info("first post filter")
+            ));
+        };
+    }
+
+}
